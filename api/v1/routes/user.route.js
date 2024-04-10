e383ac04c6d4334622658a098f6bcf757464ef1f
Chu Ngoc Thang <chuthanglsz@gmail.com>
2024-04-10 21:55:26 +0700

    Fix code

--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -10,6 +10,6 @@ router.post("/password/forgot",controller.forgotPassword);
 router.post("/password/otp",controller.otp);
 router.post("/password/reset",controller.resetPassword);
 router.get("/detail",authMiddleware.requireAuth,controller.detail);
-// router.get("/list",authMiddleware.requireAuth,controller.list);
+router.get("/list",authMiddleware.requireAuth,controller.list);
 
 module.exports=router;
